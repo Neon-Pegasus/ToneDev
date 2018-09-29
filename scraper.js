112db463244b590114261f63b0eb98d3be53796e
Ryan Heise <heis007ryan@gmail.com>
2018-09-29 17:38:41 -0500

    adds webscrape component

---
+++ scraper.js
@@ -0,0 +1,56 @@
+const puppeteer = require('puppeteer');
+
+function extractItems() {
+  const extractedElements = document.querySelectorAll('div.js-tweet-text-container');
+  const items = [];
+  for (const element of extractedElements) {
+    items.push({ tweet: element.innerText });
+  }
+  return items;
+}
+
+async function scrapeInfiniteScrollItems(
+  page,
+  extractionFunc,
+  itemTargetCount,
+  scrollDelay = 1,
+) {
+  let items = [];
+  try {
+    let previousHeight;
+    while (items.length < itemTargetCount) {
+      items = await page.evaluate(extractionFunc);
+      previousHeight = await page.evaluate('document.body.scrollHeight');
+      await page.evaluate('window.scrollTo(0, document.body.scrollHeight)');
+      await page.waitForFunction(`document.body.scrollHeight > ${previousHeight}`);
+      await page.waitFor(scrollDelay);
+    }
+  } catch (e) { console.log('error', e); }
+  return items;
+}
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
+  const page = await browser.newPage();
+  page.setViewport({ width: 1280, height: 926 });
+  await page.setRequestInterception(true);
+  page.on('request', (req) => {
+    if (req.resourceType() === 'stylesheet' || req.resourceType() === 'font' || req.resourceType() === 'image') {
+      req.abort();
+    } else {
+      req.continue();
+    }
+  });
+
+  await page.goto('https://twitter.com/google');
+
+  const items = await scrapeInfiniteScrollItems(page, extractItems, 200);
+
+  // Save extracted items to a file.
+  console.log(items);
+
+  await browser.close();
+})();
