bc04aa960b59cd84bb0a229466041955847c8e0d
JBen13 <jessicabenjaminxiii@gmail.com>
2018-09-29 20:34:54 -0700

    update gh_server to include notes.

--- server/github/gh_server.js
+++ server/github/gh_server.js
@@ -10,3 +10,13 @@ sequelize.sync().then(async () => {
     console.log(`🚀  Server ready at ${url}`);
   });
 });
+
+
+/**
+ * Apollo server set up  will require...
+ * @param { schema } 
+ * @param { resolvers }
+ * Please note that there are further parameters for setting up an Apollo Server 2.0. 
+ * The additional paramenters can be found in the API Documents at  
+ * https://www.apollographql.com/docs/apollo-server/api/apollo-server.html
+ */
