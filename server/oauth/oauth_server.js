b450239be51f846ba37c57dac7c16ae6d08cb48c
Joanne Kim <joanneskim831@gmail.com>
2018-10-01 14:57:27 -0700

    add passport middleware

--- server/oauth/oauth_server.js
+++ server/oauth/oauth_server.js
@@ -1,6 +1,32 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const GitHubStrategy = require('passport-github').Strategy;
+const passport = require('passport');
+require('dotenv').config();
 
+const port = 3333;
 const app = express();
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+passport.use(new GitHubStrategy({
+  clientID: process.env.Github_Client_ID,
+  clientSecret: process.env.Github_Client_Secret,
+  callbackURL: `http://127.0.0.1:${port}/auth/github/callback`
+},
+(accessToken, refreshToken, profile, cb) => {
+
+}));
+
+app.get('/auth/github', passport.authenticate('github'));
+
+app.get('/auth/github/callback',
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  (req, res) => {
+    res.redirect('/');
+  });
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
