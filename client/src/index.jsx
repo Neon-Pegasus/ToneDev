de030a66b8b76427500ef77bd81651907c319730
Joanne Kim <joanneskim831@gmail.com>
2018-10-02 15:10:53 -0700

    implement react router in app.jsx for home and login components

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Router, Route, Link, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Login';
 
@@ -13,7 +13,16 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        hello
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/login">Login</Link></li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path="/" Component={Home} />
+          <Route path="/login" Component={Login} />
+        </Switch>
       </div>
     );
   }
