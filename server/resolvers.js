e23e7670b91f4ebcfe6515010210ee08783ac080
Ryan Heise <heis007ryan@gmail.com>
2018-10-03 18:17:42 -0500

    installs apollo server and apollo engine running

--- server/resolvers.js
+++
@@ -1,8 +0,0 @@
-// import { User } from '../database/database';
-const { User } = require('../database/database.js');
-
-module.exports = {
-  Query: {
-    allUsers: () => User.findAll({ attributes: 'firstName' }),
-  },
-};
