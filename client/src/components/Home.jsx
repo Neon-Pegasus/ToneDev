0d1486e39c0237f8adafd13a11e6f520a5bf42f8
Jhia Turner <jhimitu@gmail.com>
2018-10-03 19:10:58 -0700

    create an organization view/component

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import {
+  Route, Link, Switch,
+} from 'react-router-dom';
+import Organizations from './Organizations';
 
 class Home extends React.Component {
   constructor() {
@@ -8,7 +12,14 @@ class Home extends React.Component {
 
   render() {
     return (
-      <div>Home</div>
+      <div>
+        <h1>Welcome to ToneDev</h1>
+        <Link to="/orgs">View Top 5 Organizations</Link>
+        <Switch>
+          <Route path="/orgs" component={Organizations} />
+        </Switch>
+
+      </div>
     );
   }
 }
---
+++ client/src/components/Organizations.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Organizations = () => (
+  <div>
+    <ul>
+      <li>Google</li>
+      <li>Twitter</li>
+      <li>Facebook</li>
+      <li>Microsoft</li>
+      <li>Amazon</li>
+    </ul>
+  </div>
+);
+
+
+export default Organizations;
