1df21533f0944098fef39aac82c353fbc6e0c466
Joanne Kim <joanneskim831@gmail.com>
2018-10-06 15:23:34 -0700

    remove oauth troubleshooting code

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,28 +1,9 @@
 import React from 'react';
-import axios from 'axios';
-
-/* window.axios = require('axios');
-
-window.axios.defaults.headers.common = {
-  'X-CSRF-TOKEN': window.Laravel.csrfToken,
-  'X-Requested-With': 'XMLHttpRequest',
-}; */
 
 class Login extends React.Component {
   constructor() {
     super();
     this.state = {};
-    this.githubLogin = this.githubLogin.bind(this);
-  }
-
-  githubLogin() {
-    // window.location = 'http://localhost:8888/auth/github';
-    this.setState({});
-    axios.get('/auth/github')
-      .then((res) => {
-        console.log(res);
-      });
-    // fetch('/auth/github')
   }
 
   render() {
@@ -31,7 +12,6 @@ class Login extends React.Component {
         Login with GitHub
         <br />
         <a href="/auth/github">Login with GitHub</a>
-        <button type="button" onClick={this.githubLogin}>Github login</button>
       </div>
     );
   }
--- server/oauth/oauth_microservice.js
+++ server/oauth/oauth_microservice.js
@@ -2,14 +2,12 @@ const express = require('express');
 const passport = require('passport');
 
 const router = express.Router();
-// passport.authenticate('github')
 
 router.get('/github', passport.authenticate('github'));
 
 router.get('/github/callback',
   passport.authenticate('github', { failureRedirect: '/login' }),
   (req, res) => {
-    console.log(req.user);
     res.setHeader('x-auth-token', req.user.token);
     res.redirect('/');
   });
