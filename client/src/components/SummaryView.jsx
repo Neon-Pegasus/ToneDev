7688e349ad942711e874acebf05dd5587655b5ff
Jhia Turner <jhimitu@gmail.com>
2018-10-09 16:55:01 -0700

    update package-lock

--- client/src/components/SummaryView.jsx
+++ client/src/components/SummaryView.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import axios from 'axios';
+import PropTypes from 'prop-types';
 import PieChart from './PieChart';
 
 
 class Summary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   componentDidMount() {
     axios.post('api/gateway/org/sentiment', {
       text: 'Here\'s a summary of changes that I incorporated from the other two answers.First, I made the red dot render correctly.Then I drew in the lines by eyeballing where the screenMinX and screenMaxX are. You may want to use a more precise measurement depending on your needs.Note thatnever existed before, so I created it to allowandto be available.',
@@ -21,10 +27,15 @@ class Summary extends React.Component {
   }
 
   render() {
+    const { orgName } = this.props;
     return (
       <div>
         <h1>Sentiment Analysis Summary</h1>
-        <h2>Overall Sentiment</h2>
+        <h2>
+Overall Sentiment
+          {' '}
+          {orgName}
+        </h2>
         <p>positive</p>
 
         <h2>Score</h2>
@@ -40,5 +51,7 @@ class Summary extends React.Component {
   }
 }
 
-
+Summary.propTypes = {
+  orgName: PropTypes.string.isRequired,
+};
 export default Summary;
