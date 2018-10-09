7d52e95a88db816dcc5cf501cc33f768520f8bda
Joanne Kim <joanneskim831@gmail.com>
2018-10-08 20:11:40 -0700

    remove router from home component

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  Link, Router,
+  Link,
 } from 'react-router-dom';
 
 
@@ -14,13 +14,11 @@ class Home extends React.Component {
     return (
       <div>
         <h1>Welcome to ToneDev</h1>
-        <Router>
-          <Link to="/orgs">View Top 5 Organizations</Link>
-          <br />
-          <Link to="/repos">View By Repo</Link>
-          <br />
-          <Link to="/user">User Stats</Link>
-        </Router>
+        <Link to="/orgs">View Top 5 Organizations</Link>
+        <br />
+        <Link to="/repos">View By Repo</Link>
+        <br />
+        <Link to="/user">User Stats</Link>
       </div>
     );
   }
