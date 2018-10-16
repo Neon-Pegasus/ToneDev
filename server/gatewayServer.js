2d56676388996ff70838b508c37d67e87ca32c15
Ryan Heise <heis007ryan@gmail.com>
2018-10-16 13:43:34 -0500

    adds server no start for tests

--- server/gatewayServer.js
+++ server/gatewayServer.js
@@ -1,7 +1,5 @@
 const { gateway } = require('./nodeGateway');
 const { port } = require('./nodeGateway');
 
-// if (process.env.NODE_ENV !== 'test') {
 gateway.server = gateway.listen(port, () => console.log(`gateway server listening on ${port}`));
-// }
 module.export = gateway;
