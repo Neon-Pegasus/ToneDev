f48ce7796c6edd57af035c83d867dda27fac7131
Jhia Turner <jhimitu@gmail.com>
2018-10-18 08:11:07 -0700

    return a sentiment analysis of user text input

---
+++ client/src/components/user_input/Input.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import axios from 'axios';
+
+class Input extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      output: {},
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const { input } = this.state;
+
+    axios.post('/api/gateway/input/sentiment', {
+      text: input,
+    })
+      .then((result) => {
+        console.log('INPUT RESULTS: ', result);
+        // this.setState({
+        //   output: result.data,
+        // });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    const { input } = this.state;
+    return (
+      <div>
+        <form onSubmit={this.onSubmit}>
+          <input
+            type="text"
+            name="input"
+            value={input}
+            onChange={this.onChange}
+          />
+          <button type="submit">Analyize</button>
+        </form>
+        {/* <p> */}
+          {/* Sentiment: {this.state.output.sentiment} */}
+          {/* {' '} */}
+          {/* Score: {this.state.output.score} */}
+        {/* </p> */}
+      </div>
+    );
+  }
+}
+
+export default Input;
---
+++ client/src/components/user_input/TextAnalysis.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Input from './Input';
+
+const TextAnalysis = () => {
+  return (
+    <div>
+      <Input />
+    </div>
+  );
+};
+
+export default TextAnalysis;
